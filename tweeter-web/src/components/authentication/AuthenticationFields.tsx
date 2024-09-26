d21ae8eda8fd993a1f0285bc78f09f24910048c2
ethanwait25 <ethanwait25@gmail.com>
2024-09-25 23:37:20 -0600

    Added AuthenticationFields to remove code duplication

---
+++ tweeter-web/src/components/authentication/AuthenticationFields.tsx
@@ -0,0 +1,38 @@
+interface Props {
+  setAlias: React.Dispatch<React.SetStateAction<string>>;
+  setPassword: React.Dispatch<React.SetStateAction<string>>;
+  onEnter: React.KeyboardEventHandler<HTMLElement>;
+  lastElement?: boolean;
+}
+
+const AuthenticationFields = (props: Props) => {
+  return (
+    <>
+      <div className="form-floating">
+        <input
+          type="text"
+          className="form-control"
+          size={50}
+          id="aliasInput"
+          placeholder="name@example.com"
+          onKeyDown={props.onEnter}
+          onChange={(event) => props.setAlias(event.target.value)}
+        />
+        <label htmlFor="aliasInput">Alias</label>
+      </div>
+      <div className={`form-floating${props.lastElement ? " mb-3" : ""}`}>
+        <input
+          type="password"
+          className={`form-control${props.lastElement ? " bottom" : ""}`}
+          id="passwordInput"
+          placeholder="Password"
+          onKeyDown={props.onEnter}
+          onChange={(event) => props.setPassword(event.target.value)}
+        />
+        <label htmlFor="passwordInput">Password</label>
+      </div>
+    </>
+  );
+};
+
+export default AuthenticationFields;
