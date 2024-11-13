1e424f2994a17dc8d2ae675fc3e9cc449b362651
ethanwait25 <ethanwait25@gmail.com>
2024-11-13 16:48:58 -0700

    All StatusService methods now call server through ServerFacade

--- tweeter-web/src/model/service/ServerFacade.ts
+++ tweeter-web/src/model/service/ServerFacade.ts
@@ -9,6 +9,8 @@ import {
   UserDTO,
   TweeterResponse,
   CountsResponse,
+  StatusDTO,
+  Status,
 } from "tweeter-shared";
 import { ClientCommunicator } from "./ClientCommunicator";
 import { URL } from "../../../serverconfig.json";
@@ -54,9 +56,7 @@ export class ServerFacade {
     }
   }
 
-  public async getFolloweeCount(
-    request: DTORequest<UserDTO>
-  ): Promise<number> {
+  public async getFolloweeCount(request: DTORequest<UserDTO>): Promise<number> {
     const response = await this.clientCommunicator.doPost<
       DTORequest<UserDTO>,
       CountResponse
@@ -70,9 +70,7 @@ export class ServerFacade {
     }
   }
 
-  public async getFollowerCount(
-    request: DTORequest<UserDTO>
-  ): Promise<number> {
+  public async getFollowerCount(request: DTORequest<UserDTO>): Promise<number> {
     const response = await this.clientCommunicator.doPost<
       DTORequest<UserDTO>,
       CountResponse
@@ -118,6 +116,40 @@ export class ServerFacade {
     }
   }
 
+  public async getMoreFeedItems(
+    request: PagedItemRequest<StatusDTO>
+  ): Promise<[Status[], boolean]> {
+    const response = await this.clientCommunicator.doPost<
+      PagedItemRequest<StatusDTO>,
+      PagedItemResponse<StatusDTO>
+    >(request, "/status/feedlist");
+    return await this.returnStatusList(response, "feed");
+  }
+
+  public async getMoreStoryItems(
+    request: PagedItemRequest<StatusDTO>
+  ): Promise<[Status[], boolean]> {
+    const response = await this.clientCommunicator.doPost<
+      PagedItemRequest<StatusDTO>,
+      PagedItemResponse<StatusDTO>
+    >(request, "/status/storylist");
+    return await this.returnStatusList(response, "story");
+  }
+
+  public async postStatus(request: DTORequest<StatusDTO>): Promise<void> {
+    const response = await this.clientCommunicator.doPost<
+      DTORequest<StatusDTO>,
+      TweeterResponse
+    >(request, "/status/create");
+
+    if (response.success) {
+      return;
+    } else {
+      console.error(response);
+      throw new Error(response.message ?? undefined);
+    }
+  }
+
   private async returnUsersList(
     response: PagedItemResponse<UserDTO>,
     target: string
@@ -132,6 +164,29 @@ export class ServerFacade {
     if (response.success) {
       if (items == null) {
         throw new Error(`No ${target} found`);
+      } else {
+        return [items, response.hasMore];
+      }
+    } else {
+      console.error(response);
+      throw new Error(response.message ?? undefined);
+    }
+  }
+
+  private async returnStatusList(
+    response: PagedItemResponse<StatusDTO>,
+    target: string
+  ): Promise<[Status[], boolean]> {
+    // Convert the StatusDTO array returned by ClientCommunicator to a Status array
+    const items: Status[] | null =
+      response.success && response.items
+        ? response.items.map((dto) => Status.fromDTO(dto) as Status)
+        : null;
+
+    // Handle errors
+    if (response.success) {
+      if (items == null) {
+        throw new Error(`No ${target} items found`);
       } else {
         return [items, response.hasMore];
       }
--- tweeter-web/src/model/service/StatusService.ts
+++ tweeter-web/src/model/service/StatusService.ts
@@ -1,14 +1,21 @@
-import { AuthToken, FakeData, Status } from "tweeter-shared";
+import { AuthToken, Status } from "tweeter-shared";
+import { ServerFacade } from "./ServerFacade";
 
 export class StatusService {
+  facade = new ServerFacade();
+
   public async loadMoreFeedItems(
     authToken: AuthToken,
     userAlias: string,
     pageSize: number,
     lastItem: Status | null
   ): Promise<[Status[], boolean]> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+    return this.facade.getMoreFeedItems({
+      token: authToken.token,
+      userAlias: userAlias,
+      pageSize: pageSize,
+      lastItem: lastItem ? lastItem.dto : null,
+    });
   }
 
   public async loadMoreStoryItems(
@@ -17,17 +24,21 @@ export class StatusService {
     pageSize: number,
     lastItem: Status | null
   ): Promise<[Status[], boolean]> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+    return this.facade.getMoreStoryItems({
+      token: authToken.token,
+      userAlias: userAlias,
+      pageSize: pageSize,
+      lastItem: lastItem ? lastItem.dto : null,
+    });
   }
 
   public async postStatus(
     authToken: AuthToken,
     newStatus: Status
   ): Promise<void> {
-    // Pause so we can see posting message. Remove when connected to the server
-    await new Promise((f) => setTimeout(f, 2000));
-
-    // TODO: Call the server to post the status
+    return this.facade.postStatus({
+      token: authToken.token,
+      dto: newStatus.dto,
+    });
   }
 }
