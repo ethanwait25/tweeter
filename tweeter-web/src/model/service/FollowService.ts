f235d86f5a6d345cb4bb892c762fc44136d8b960
ethanwait25 <ethanwait25@gmail.com>
2024-11-13 16:18:08 -0700

    All FollowService methods now call server through ServerFacade

--- tweeter-web/src/model/service/FollowService.ts
+++ tweeter-web/src/model/service/FollowService.ts
@@ -1,4 +1,4 @@
-import { AuthToken, FakeData, User, PagedItemRequest } from "tweeter-shared";
+import { AuthToken, User } from "tweeter-shared";
 import { ServerFacade } from "./ServerFacade";
 
 export class FollowService {
@@ -10,8 +10,12 @@ export class FollowService {
     pageSize: number,
     lastItem: User | null
   ): Promise<[User[], boolean]> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+    return this.facade.getMoreFollowers({
+      token: authToken.token,
+      userAlias: userAlias,
+      pageSize: pageSize,
+      lastItem: lastItem ? lastItem.dto : null,
+    });
   }
 
   public async loadMoreFollowees(
@@ -33,59 +37,50 @@ export class FollowService {
     user: User,
     selectedUser: User
   ): Promise<boolean> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.isFollower();
+    return this.facade.getIsFollowerStatus({
+      token: authToken.token,
+      user: user.dto,
+      selectedUser: selectedUser.dto
+    });
   }
 
   public async getFolloweeCount(
     authToken: AuthToken,
     user: User
   ): Promise<number> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getFolloweeCount(user.alias);
+    return this.facade.getFolloweeCount({
+      token: authToken.token,
+      dto: user.dto
+    });
   }
 
   public async getFollowerCount(
     authToken: AuthToken,
     user: User
   ): Promise<number> {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getFollowerCount(user.alias);
+    return this.facade.getFollowerCount({
+      token: authToken.token,
+      dto: user.dto
+    });
   }
 
   public async follow(
     authToken: AuthToken,
     userToFollow: User
   ): Promise<[followerCount: number, followeeCount: number]> {
-    // Pause so we can see the follow message. Remove when connected to the server
-    await new Promise((f) => setTimeout(f, 2000));
-
-    // TODO: Call the server
-
-    const followerCount = await this.getFollowerCount(authToken, userToFollow);
-    const followeeCount = await this.getFolloweeCount(authToken, userToFollow);
-
-    return [followerCount, followeeCount];
+    return this.facade.follow({
+      token: authToken.token,
+      dto: userToFollow.dto
+    });
   }
 
   public async unfollow(
     authToken: AuthToken,
     userToUnfollow: User
   ): Promise<[followerCount: number, followeeCount: number]> {
-    // Pause so we can see the unfollow message. Remove when connected to the server
-    await new Promise((f) => setTimeout(f, 2000));
-
-    // TODO: Call the server
-
-    const followerCount = await this.getFollowerCount(
-      authToken,
-      userToUnfollow
-    );
-    const followeeCount = await this.getFolloweeCount(
-      authToken,
-      userToUnfollow
-    );
-
-    return [followerCount, followeeCount];
+    return this.facade.unfollow({
+      token: authToken.token,
+      dto: userToUnfollow.dto
+    });
   }
 }
--- tweeter-web/src/model/service/ServerFacade.ts
+++ tweeter-web/src/model/service/ServerFacade.ts
@@ -1,41 +1,143 @@
 import {
-    PagedItemRequest,
-    PagedItemResponse,
-    User,
-    UserDTO
-  } from "tweeter-shared";
-  import { ClientCommunicator } from "./ClientCommunicator";
-  import { URL } from "../../../serverconfig.json";
-  
-  export class ServerFacade {
-    private SERVER_URL = URL;
-  
-    private clientCommunicator = new ClientCommunicator(this.SERVER_URL);
-  
-    public async getMoreFollowees(
-      request: PagedItemRequest<UserDTO>
-    ): Promise<[User[], boolean]> {
-      const response = await this.clientCommunicator.doPost<
-        PagedItemRequest<UserDTO>,
-        PagedItemResponse<UserDTO>
-      >(request, "/followee/list");
-  
-      // Convert the UserDTO array returned by ClientCommunicator to a User array
-      const items: User[] | null =
-        response.success && response.items
-          ? response.items.map((dto) => User.fromDTO(dto) as User)
-          : null;
-  
-      // Handle errors
-      if (response.success) {
-        if (items == null) {
-          throw new Error(`No followees found`);
-        } else {
-          return [items, response.hasMore];
-        }
-      } else {
-        console.error(response);
-        throw new Error(response.message ?? undefined);
-      }
+  PagedItemRequest,
+  FollowerStatusRequest,
+  DTORequest,
+  PagedItemResponse,
+  FollowerStatusResponse,
+  CountResponse,
+  User,
+  UserDTO,
+  TweeterResponse,
+  CountsResponse,
+} from "tweeter-shared";
+import { ClientCommunicator } from "./ClientCommunicator";
+import { URL } from "../../../serverconfig.json";
+
+export class ServerFacade {
+  private SERVER_URL = URL;
+
+  private clientCommunicator = new ClientCommunicator(this.SERVER_URL);
+
+  public async getMoreFollowees(
+    request: PagedItemRequest<UserDTO>
+  ): Promise<[User[], boolean]> {
+    const response = await this.clientCommunicator.doPost<
+      PagedItemRequest<UserDTO>,
+      PagedItemResponse<UserDTO>
+    >(request, "/followee/list");
+    return await this.returnUsersList(response, "followees");
+  }
+
+  public async getMoreFollowers(
+    request: PagedItemRequest<UserDTO>
+  ): Promise<[User[], boolean]> {
+    const response = await this.clientCommunicator.doPost<
+      PagedItemRequest<UserDTO>,
+      PagedItemResponse<UserDTO>
+    >(request, "/follower/list");
+    return await this.returnUsersList(response, "followers");
+  }
+
+  public async getIsFollowerStatus(
+    request: FollowerStatusRequest
+  ): Promise<boolean> {
+    const response = await this.clientCommunicator.doPost<
+      FollowerStatusRequest,
+      FollowerStatusResponse
+    >(request, "/follower/status");
+
+    if (response.success) {
+      return response.isFollower;
+    } else {
+      console.error(response);
+      throw new Error(response.message ?? undefined);
     }
   }
+
+  public async getFolloweeCount(
+    request: DTORequest<UserDTO>
+  ): Promise<number> {
+    const response = await this.clientCommunicator.doPost<
+      DTORequest<UserDTO>,
+      CountResponse
+    >(request, "/followee/count");
+
+    if (response.success) {
+      return response.count;
+    } else {
+      console.error(response);
+      throw new Error(response.message ?? undefined);
+    }
+  }
+
+  public async getFollowerCount(
+    request: DTORequest<UserDTO>
+  ): Promise<number> {
+    const response = await this.clientCommunicator.doPost<
+      DTORequest<UserDTO>,
+      CountResponse
+    >(request, "/follower/count");
+
+    if (response.success) {
+      return response.count;
+    } else {
+      console.error(response);
+      throw new Error(response.message ?? undefined);
+    }
+  }
+
+  public async follow(
+    request: DTORequest<UserDTO>
+  ): Promise<[followerCount: number, followeeCount: number]> {
+    const response = await this.clientCommunicator.doPost<
+      DTORequest<UserDTO>,
+      CountsResponse
+    >(request, "/user/follow");
+
+    if (response.success) {
+      return [response.followerCount, response.followeeCount];
+    } else {
+      console.error(response);
+      throw new Error(response.message ?? undefined);
+    }
+  }
+
+  public async unfollow(
+    request: DTORequest<UserDTO>
+  ): Promise<[followerCount: number, followeeCount: number]> {
+    const response = await this.clientCommunicator.doPost<
+      DTORequest<UserDTO>,
+      CountsResponse
+    >(request, "/user/unfollow");
+
+    if (response.success) {
+      return [response.followerCount, response.followeeCount];
+    } else {
+      console.error(response);
+      throw new Error(response.message ?? undefined);
+    }
+  }
+
+  private async returnUsersList(
+    response: PagedItemResponse<UserDTO>,
+    target: string
+  ): Promise<[User[], boolean]> {
+    // Convert the UserDTO array returned by ClientCommunicator to a User array
+    const items: User[] | null =
+      response.success && response.items
+        ? response.items.map((dto) => User.fromDTO(dto) as User)
+        : null;
+
+    // Handle errors
+    if (response.success) {
+      if (items == null) {
+        throw new Error(`No ${target} found`);
+      } else {
+        return [items, response.hasMore];
+      }
+    } else {
+      console.error(response);
+      throw new Error(response.message ?? undefined);
+    }
+  }
+}
